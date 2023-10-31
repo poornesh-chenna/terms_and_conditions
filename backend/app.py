11a59cb8c4b65ddd891edb75968b4d4ce285dfd0
LeelaRohith <leelarohith10@gmail.com>
2023-10-31 18:17:23 +0530

    app.py updated

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,35 @@
+
 from flask import Flask
+import pickle
+import joblib
 app = Flask(__name__)
+
 
 @app.get('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.post('/summary')
 def get():
-    sentences = summarized(source)
-    load 
+    sentences = summarize(source)
+    l = joblib.load('Text_SVM.pkl')
+    count_vect = l[1]
+    transformer = l[2]
+    model = l[0]
+    d = {}
     for sentence in sentences:
-        predict(sentence)
+        mc = count_vect.transform([sentence])
+        m = transformer.transform(mc)
+        output = model.predict(m)
+        if output[0] not in d.keys():
+            newlist = list()
+            newlist.append(sentence)
+            d[output[0]] = newlist
+        else:
+            d[output[0]].append(sentence)
+    return d
+
 
 if __name__ == '__main__':
     app.run()
